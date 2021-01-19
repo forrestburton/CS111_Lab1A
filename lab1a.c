1529d44003be004f65f2cec1465d50eceb084f23
Forrest Burton <burton.forrest10@gmail.com>
2021-01-19 14:10:45 -0800

    started implementation of shell option. fork, exec

--- lab1a.c
+++ lab1a.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 struct termios normal_mode;
 
@@ -38,6 +39,8 @@ void reset_terminal(struct termios normal_mode) {
 
 int main(int argc, char *argv[]) {
     int c;
+    int shell_opt = 0;
+    int debug_opt = 0;
     while(1) {
         int option_index = 0;
         static struct option long_options[] = {
@@ -48,16 +51,48 @@ int main(int argc, char *argv[]) {
         if (c == -1) break;
         switch (c) {
             case 's':
-                //shell option
+                shell_opt = 1;
                 break;
             case 'd':
-                //debug option
+                debug_opy = 1;
                 break;
             default:
                 printf("Incorrect usage: accepted options are: [--shell --debug]\n");
                 exit(1);
         }
     }
+    //Shell option
+    if (shell_opt) {
+        setup_terminal_mode();
+
+        int ret = fork(); //create a child process from main
+
+        if (ret < 0) {
+            fprintf(stderr, "Error when forking main: %s\n", strerror(errno));
+            exit(1);
+        }
+        else if (ret == 0) {  //child process will have return value of 0. Output is by default nondeterministic. We don't know order of execution
+            
+            char* args_exec[] = {"/bin/bash", NULL};
+            ret = execvp(args_exec[0], args_exec);  //executing a new (shell) program: /bin/bash
+            if (ret == -1) {
+                fprintf(stderr, "Error when executing execvp in child process: %s\n", strerror(errno));
+                exit(1);
+            }
+            exit(5);
+        }
+        else if (ret > 0) {  //parent process will have return value of > 0
+            
+
+            int exit_status;
+            waitpid(ret, &exit_status, 0);  //wait for child process to finish
+            printf("Child process is exiting. Exit code: %d\n", WEXITSTATUS(exit_status));
+            exit(0);
+        }
+    }
+
+
+    //Default execution(no options given)
     setup_terminal_mode();  //setup terminal and save original state
 
     char buffer;
@@ -85,9 +120,7 @@ int main(int argc, char *argv[]) {
             else {
                 write(1, &buffer, sizeof(char));
             }
-
         }
     }
-
     exit(0);
 }
