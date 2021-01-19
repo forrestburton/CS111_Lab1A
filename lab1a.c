1abf8b74e156c3399a1df699c8aa91414d14ad6d
Forrest Burton <burton.forrest10@gmail.com>
2021-01-19 12:37:36 -0800

    reading/writing from stdin complete

--- lab1a.c
+++ lab1a.c
@@ -18,17 +18,26 @@ struct termios normal_mode;
 void setup_terminal_mode(void) {
     //save current mode so we can restore it at the end 
     int error_check = tcgetattr(0, &normal_mode);  
+    struct termios new_mode = normal_mode;
     if (error_check < 0) {
-        fprintf(stderr, "Error: %s\n", strerror(errno));
-        exit(1)
+        fprintf(stderr, "Error setting up terminal mode: %s\n", strerror(errno));
+        exit(1);
     }
     //setting terminal to non-cononical, non-echo mode(letters aren't echoed)
-    tmp.c_iflag = ISTRIP, tmp.c_oflag = 0, tmp.c_lflag = 0;
-    struct termios new_mode = normal_mode;
+    new_mode.c_iflag = ISTRIP, new_mode.c_oflag = 0, new_mode.c_lflag = 0;
     tcsetattr(0, TCSANOW, &new_mode);
 }
 
+void reset_terminal(struct termios normal_mode) {
+    int error_check = tcsetattr(0, TCSANOW, &normal_mode);
+    if (error_check < 0) {
+        fprintf(stderr, "Error restoring terminal mode: %s\n", strerror(errno));
+        exit(1);
+    } 
+}
+
 int main(int argc, char *argv[]) {
+    int c;
     while(1) {
         int option_index = 0;
         static struct option long_options[] = {
@@ -49,6 +58,36 @@ int main(int argc, char *argv[]) {
                 exit(1);
         }
     }
-    
-    setup_terminal_mode();
+    setup_terminal_mode();  //setup terminal and save original state
+
+    char buffer;
+    ssize_t ret;
+    //read (ASCII) input from keyboard into buffer
+    while(1) {
+        ret = read(0, &buffer, sizeof(char));  //read bytes from stdin
+        if(ret < 0) {  
+            fprintf(stderr, "Error reading from standard input: %s\n", strerror(errno));
+            exit(1);
+        }
+
+        for (int i = 0; i < ret; i++) { //for each char we read in the buffer
+            if (buffer == 0x4) {
+                write(1, "^D", 2*sizeof(char));
+                reset_terminal(normal_mode);
+                exit(0);
+            }
+            else if (buffer == '\r' || buffer == '\n') {
+                write(1, "\r\n", 2*sizeof(char));
+            }
+            else if (buffer == 0x3) {
+                write(1, "^C", 2*sizeof(char));
+            }
+            else {
+                write(1, &buffer, sizeof(char));
+            }
+
+        }
+    }
+
+    exit(0);
 }
